6dd0e40630c38224c479dc59d7332257451578e7
Rafal Pajdak <rpajdak@gmail.com>
2021-03-03 18:27:59 +0100

    Init commit

---
+++ src/main/java/com/rpajdak/genderdetector/GenderDetecorApplication.java
@@ -0,0 +1,13 @@
+package com.rpajdak.genderdetector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenderDetecorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenderDetecorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rpajdak/genderdetector/GenderDetecorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rpajdak.genderdetector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenderDetecorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
