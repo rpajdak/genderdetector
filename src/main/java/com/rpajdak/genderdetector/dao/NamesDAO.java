f43dc430e90827b4ed343db807d00e65d7922b34
Rafal Pajdak <rpajdak@gmail.com>
2021-03-04 10:59:26 +0100

    Move NamesDAO to another package.

--- src/main/java/com/rpajdak/genderdetector/dao/NamesDAO.java
+++ src/main/java/com/rpajdak/genderdetector/dao/NamesDAO.java
@@ -1,9 +1,9 @@
-package com.rpajdak.genderdetector.controller.dao;
+package com.rpajdak.genderdetector.dao;
 
 public interface NamesDAO {
 
     String getAllFemaleNames();
 
     String geAllMaleNames();
-    
+
 }
