8e2d3dd919c6a7f6358f7f983e4c856e04237c69
Rafal Pajdak <rpajdak@gmail.com>
2021-03-04 12:51:26 +0100

    Add two methods to Names interface- to get male and female names scanner.

--- src/main/java/com/rpajdak/genderdetector/dao/NamesDAO.java
+++ src/main/java/com/rpajdak/genderdetector/dao/NamesDAO.java
@@ -1,9 +1,14 @@
 package com.rpajdak.genderdetector.dao;
 
+import java.util.Scanner;
+
 public interface NamesDAO {
 
     String getAllFemaleNames();
 
     String geAllMaleNames();
 
+    Scanner getScannerOfFemaleNames();
+
+    Scanner getScannerOfMalesNames();
 }
