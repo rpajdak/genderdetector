0274bbd17c2570999017edd3b4aad98df7b05b0e
Rafal Pajdak <rpajdak@gmail.com>
2021-03-06 14:10:46 +0100

    Remove typo in class name

--- src/main/java/com/rpajdak/genderdetector/GenderDetectorApplication.java
+++ src/main/java/com/rpajdak/genderdetector/GenderDetectorApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GenderDetecorApplication {
+public class GenderDetectorApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GenderDetecorApplication.class, args);
+        SpringApplication.run(GenderDetectorApplication.class, args);
     }
 
 }
